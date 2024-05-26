a3b18f4fd511db4765fda7cf341edec6db0c1e59
HilmyAmmar <hilmyammardarmawan17@gmail.com>
2024-05-26 21:37:55 +0700

    Implement DIP

--- src/main/java/id/ac/ui/cs/advprog/product_service/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/product_service/controller/ProductController.java
@@ -1,7 +1,9 @@
 package id.ac.ui.cs.advprog.product_service.controller;
 
 import id.ac.ui.cs.advprog.product_service.model.Product;
-import id.ac.ui.cs.advprog.product_service.service.ProductService;
+import id.ac.ui.cs.advprog.product_service.service.ProductMarketInterface;
+import id.ac.ui.cs.advprog.product_service.service.ProductReadInterface;
+import id.ac.ui.cs.advprog.product_service.service.ProductWriteInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,23 +16,30 @@ import java.util.Optional;
 @RequestMapping("/products")
 public class ProductController {
     @Autowired
-    private ProductService productService;
+    private ProductReadInterface productReadInterface;
+
+    @Autowired
+    private ProductWriteInterface productWriteInterface;
+
+    @Autowired
+    private ProductMarketInterface productMarketInterface;
+
     @GetMapping
     public ResponseEntity<Object> findAllProduct() {
-        List<Product> productList = productService.findAll();
+        List<Product> productList = productReadInterface.findAll();
         return new ResponseEntity<>(productList, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> findProductById(@PathVariable Long id) {
-        Optional<Product> product = productService.findById(id);
+        Optional<Product> product = productReadInterface.findById(id);
         return product.<ResponseEntity<Object>>map(value -> new ResponseEntity<>(value, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/market/{supermarketId}")
     public ResponseEntity<Object> findProductByMarket(@PathVariable Long supermarketId) {
-        List<Product> productList = productService.findByMarketId(supermarketId);
+        List<Product> productList = productMarketInterface.findByMarketId(supermarketId);
         if (productList.isEmpty()) {
             return new ResponseEntity<>("No products found for market with id: " + supermarketId, HttpStatus.NOT_FOUND);
         } else {
@@ -41,7 +50,7 @@ public class ProductController {
     @PostMapping("/addProducts")
     public ResponseEntity<Object> addProduct(@RequestBody Product product) {
         try {
-            Product addedProduct = productService.addProduct(product);
+            Product addedProduct = productWriteInterface.addProduct(product);
             return new ResponseEntity<>(addedProduct, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>("Failed to add product: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
@@ -51,7 +60,7 @@ public class ProductController {
     @PutMapping("/updateProduct/{id}")
     public ResponseEntity<Object> updateProduct(@PathVariable Long id, @RequestBody Product updatedProduct) {
         try {
-            Product product = productService.updateProduct(id, updatedProduct);
+            Product product = productWriteInterface.updateProduct(id, updatedProduct);
             return new ResponseEntity<>(product, HttpStatus.OK);
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
@@ -61,11 +70,10 @@ public class ProductController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteProductById(@PathVariable Long id) {
         try {
-            productService.deleteProductById(id);
+            productWriteInterface.deleteProductById(id);
             return new ResponseEntity<>("Product deleted successfully", HttpStatus.OK);
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 }
-
